b01a58c78539d45b1f5a0da0d0beaeb21b3c7569
Warnamal <lwarnamal@gmail.com>
2024-02-24 14:18:19 +0530

    More details of product

--- functions/common_function.php
+++ functions/common_function.php
@@ -32,7 +32,7 @@ function getproducts()
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='#' class='btn btn-secondary'>View more</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
               </div>
             </div>
           </div> ";
@@ -70,7 +70,7 @@ function get_all_products()
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='#' class='btn btn-secondary'>View more</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
               </div>
             </div>
           </div> ";
@@ -111,7 +111,7 @@ function get_uniqe_categories()
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='#' class='btn btn-secondary'>View more</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
               </div>
             </div>
           </div> ";
@@ -151,7 +151,7 @@ function get_uniqe_brands()
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='#' class='btn btn-secondary'>View more</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
               </div>
             </div>
           </div> ";
@@ -224,7 +224,7 @@ function search_products()
                 <h5 class='card-title'>$product_title</h5>
                 <p class='card-text'>$product_description</p>
                 <a href='#' class='btn btn-info'>Add to cart</a>
-                <a href='#' class='btn btn-secondary'>View more</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
               </div>
             </div>
           </div> ";
---
+++ product_details.php
@@ -0,0 +1,163 @@
+<!-- connect file -->
+<?php
+// Include the file that establishes the database connection
+include('includes/connect.php');
+include('functions/common_function.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ecommerce Website</title>
+  <!-- bootstrap CSS link -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- font awesome link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- css file -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <!-- navbar -->
+  <div class="container-fluid p-0">
+    <!-- first child -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-info">
+      <div class="container-fluid">
+        <img src="./images/logo.png" alt="" class="logo">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="display_all.php">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contect</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+            </li>
+          </ul>
+            <form class="d-flex" action="search_product.php" method="GET">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
+              <!-- <button class="btn btn-outline-dark" type="submit">Search</button> -->
+              <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product">
+            </form>
+        </div>
+      </div>
+    </nav>
+
+    <!-- second child -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Welcome Guest</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- second child -->
+    <div class="bg-light">
+      <h3 class="text-center">Hidden Store</h3>
+      <p class="text-center">Communication is at the heart of e-commerce and community</p>
+    </div>
+
+    <!-- third child -->
+    <div class="row px-1">
+      <div class="col-md-10">
+        <!-- products -->
+        <div class="row">
+            <div class="col-md-4">
+                <!-- card -->
+                <div class='card'>
+              <img src='./images/orange.png' class='card-img-top' alt='$product_title'>
+              <div class='card-body'>
+                <h5 class='card-title'>$product_title</h5>
+                <p class='card-text'>$product_description</p>
+                <a href='#' class='btn btn-info'>Add to cart</a>
+                <a href='product_details.php?product_id=$product_id' class='btn btn-secondary'>View more</a>
+              </div>
+            </div>
+            </div>
+
+            <div class="col-md-8">
+                <!-- related images -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-center text-info mb-5">Related products</h4>
+                    </div>
+                    <div class="col-md-6">
+                        <img src='./images/orange1.png' class='card-img-top' alt='$product_title'>
+                    </div>
+                    <div class="col-md-6">
+                        <img src='./images/orange2.png' class='card-img-top' alt='$product_title'>
+                    </div>
+                </div>
+            </div>
+
+          <!-- fetching products -->
+          <?php
+          // calling function
+          get_uniqe_categories();
+          get_uniqe_brands();
+          ?>
+        </div>
+      </div>
+
+      <div class="col-md-2 bg-secondary p-0">
+
+        <!-- brands to be displayed -->
+        <ul class="navbar-nav me-auto text-center">
+          <li class="nav-item bg-info">
+            <a class="nav-link text-light" href="#">
+              <h4>Delivery Brand</h4>
+            </a>
+          </li>
+          <?php
+          // calling function
+          getbrands()
+          ?>
+        </ul>
+
+        <!-- categories to be displayed -->
+        <ul class="navbar-nav me-auto text-center">
+          <li class="nav-item bg-info">
+            <a class="nav-link text-light" href="#">
+              <h4>Categories</h4>
+            </a>
+          </li>
+
+          <?php
+          // calling function
+          getcategories();
+          ?>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- footer -->
+  <!-- include footer -->
+  <?php include("./includes/footer.php") ?>
+
+  <!-- bootstrap js link -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
